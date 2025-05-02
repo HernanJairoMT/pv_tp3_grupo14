ef771c0c0f25f97684cf96e723cba01f3ac6f8e7
CarlosFigueroa497 <carlos.4760230@gmail.com>
2025-05-01 22:07:23 -0300

    Agregue estilos ( tachado)  y edite boton del menu

--- src/components/TaskItem.jsx
+++ src/components/TaskItem.jsx
@@ -1,15 +1,18 @@
 function TaskItem({ task, onToggleComplete, onDelete }) {
   return (
     <li className="task-item">
-      <span>{task.text}</span>
+      <span>• </span>
+      <span style={{ textDecoration: task.completed ? 'line-through' : 'none', color: task.completed ? '#888' : 'white' }}>
+        {task.text}
+      </span>
       <div className="task-buttons">
         <button onClick={() => onToggleComplete(task.id)}>
-          {task.completed ? 'Desmarcar' : 'Completar'}
+          {task.completed ? 'Realizada' : 'Completar'}
         </button>
         <button onClick={() => onDelete(task.id)}>Eliminar</button>
       </div>
     </li>
   );
 }
-  export default TaskItem;
-  
+
+export default TaskItem;
